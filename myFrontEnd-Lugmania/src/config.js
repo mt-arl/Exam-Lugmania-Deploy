d517f3afd3ee8df44bd0985457403c7f127478c2
mt-arl <matt_ariel@hotmail.com>
2025-03-10 00:05:29 -0500

    Corregir la URL base de la API en la configuración del frontend

--- myFrontEnd-Lugmania/src/config.js
+++ myFrontEnd-Lugmania/src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'https://backend-exsam-lugmania-deploy.vercel.app';
+export const API_BASE_URL = 'https://backend-exam-lugmania-deploy.vercel.app';
 
 //export const API_BASE_URL = 'http://localhost:3011';
